c67b9daf24cdba715104f1586550cb2a68b5601f
robin <robinnagpal.tiet@gmail.com>
2021-02-07 14:57:20 -0500

    Add the first example

--- cdk_intro/lib/cdk_intro-stack.ts
+++ cdk_intro/lib/cdk_intro-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class CdkIntroStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
     // The code that defines your stack goes here
   }
 }
